708e1341772233cac6da2da8bc40645b4c4734b6
lboyd002 <lboyd002@ucr.edu>
2015-04-17 16:42:22 -0700

    added strtok to break up tokens

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,71 @@
 #include <string>
 #include <stdlib.h>	//exit
 #include <stdio.h>	//perror
+#include <errno.h>	//perror
+#include <sys/types.h>	//wait
 #include <sys/wait.h>	//wait
 #include <unistd.h>	//execvp, fork, gethost, getlogin
 #include <string.h>	//strtok
 using namespace std;
 
-int main(int argc, char *argv[]){
+int main(/*int argc, char *argv[]*/){	//can not figure out how to use argv
 	
-	//getting login info
 	char *login = getlogin();
+	
 	if(login == NULL){
 		perror("get login");
 	}
+	//char host[100];
 	
-	string cmndline;
+	//if(getlogin_r(login, sizeof(user)-1)){
+	//	perror("Error with getlogin");
+	//}
+	
+	
+	//if(-1 == gethostname(host,sizeof(host)-1)){
+        //	perror("Error with gethostname.");
+	//}
+	
+	//string cmndline;
 	//execute
 	while(1){
 		
-		//string cmndline;
-		cout << login << " $ ";
-		//get the command line
-		getline(cin, cmndline);
+		string input;
+		char cmndline[1024];
 		
-		//cout << cmndline;
+		cout << login <<  "$ ";
+		
+		//get the input from user
+		getline(cin, input);
+		
+		//char *cstr[input.length()+1];
+		//change input from string to cstring
+		strcpy(cmndline, input.c_str());
 			
-		if(cmndline == "exit")
-			exit(0);
+		if(!strcmp(cmndline, "exit"))
+			exit(1);	
 		
-		
+		//tokenize the commandline
+		char *Tok;
+		char delim[]= " ;&|";
+		Tok=strtok(cmndline, delim);
+			
+		//cout << Tok ;
+		//use argv to store the tokens in
+			
 		int i = fork();
 		if(i==0) {
 			//error check execvp
 			if (-1==execvp(argv[0], argv)) {
-				perror("execvp");
+				perror("execvp didn't work correctly");
 			}
-			//in child
-			else
-				execvp(argv[0],argv);
 		}
 		//in parent
 		else{
-			wait(0);
-			cout << "HI!";
+			if(wait(0)==-1)
+				perror("Error in wait");
+			cout << "In parent waiting.";
 		}
-	}
-	
+	}	
 	return 0;
 }
