8ff10063abfdc55e9da910617f169e48fbaa16ef
lboyd002 <lboyd002@ucr.edu>
2015-04-23 11:00:29 -0700

    started ls.cpp with checking args

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
+	vector<string> dir_names;
+	int fileNumber = 1;
+	//set flag as you check which kind of argument is input
+	int flag=0;
+	//make first spot in dir_names '.'
+	dir_names.push_back(".");
 	
-	cout << "hello" << endl;	
+	//check if the arguments are flags or filenames
+	//check online for flag bits	
+	
 	
 	return 0;
 }
