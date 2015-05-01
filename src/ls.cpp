4d42834ff0767801ca8daa3f104929ac6201c867
lboyd002 <lboyd002@ucr.edu>
2015-04-30 17:02:01 -0700

    fixed -l, info not right

--- src/ls.cpp
+++ src/ls.cpp
@@ -14,6 +14,7 @@
 
 using namespace std;
 
+void permission(const struct stat buf, dirent *dirp);
 void ls_a(const char* path){
 	DIR *dirp;
 	if(NULL == (dirp = opendir(path))){
@@ -37,16 +38,20 @@ void ls_a(const char* path){
 	return;
 }
 
-void ls_l(const char* name, struct stat s){
+void ls_l(const char* dir){
 	DIR *dirp;
-	if(NULL == (dirp = opendir(path))){
+	if(NULL == (dirp = opendir(dir))){
 		perror("There was an error with opendir().");
 		exit(1);
 	}
 	struct dirent *filespecs;
+	struct stat s;
+	//stat(dir, &s);
 	errno = 0;
 	while( (filespecs = readdir(dirp) ) != NULL){
-		cout << filespecs -> d_name << "  " ;
+		stat(dir, &s);
+		permission(s, filespecs);
+		//cout << endl;
 	}
 	if(errno != 0){
 		perror("There was an error with readdir().");
@@ -61,7 +66,12 @@ void ls_l(const char* name, struct stat s){
 	return;
 }
 
-void permission(const struct stat buf){
+void permission(const struct stat buf, dirent *dirp){
+	struct tm* file_t;
+	file_t = localtime( &buf.st_mtime);
+	char buffer[100];
+	strftime(buffer, 100, "%h %e %R", file_t);
+	
 	(buf.st_mode & S_IFDIR)? cout << "d":
 	(buf.st_mode & S_IFCHR) ? cout<< 'c':
 	(buf.st_mode & S_IFBLK) ? cout<< 'b':
@@ -81,12 +91,16 @@ void permission(const struct stat buf){
 	(buf.st_mode & S_IWOTH)? cout << "w": cout << "-";
 	(buf.st_mode & S_IXOTH)? cout << "x": cout << "-";
 	cout << " ";
+	
+	cout << " " << buf.st_nlink << " ";
+	cout << getpwuid(buf.st_uid)-> pw_name << " ";
+	cout << getgrgid(buf.st_gid) -> gr_name << " ";
+	cout << buf.st_size << " " << buffer << " " << dirp->d_name << endl;
 }
 
 int main(int argc, char* argv[]){
 	vector<char*> dir_names;
 	vector<char*> file_names;
-	struct stat s;
 	//char* path;
 	
 	//set flag as you check which kind of argument is input
@@ -106,8 +120,8 @@ int main(int argc, char* argv[]){
 		}
 		else if(!strcmp(argv[pos], "-l")){ 
 			flag = flag | 0x02;
-			if(argc < = 2)
-				ls_l(".", s);
+			if(argc <= 2)
+				ls_l(".");
 		}
 		else if(!strcmp(argv[pos], "-R")){
 			flag = flag | 0x04;
