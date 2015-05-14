e0c76be6da10554d667f21b65b4bdec95263b9cb
lboyd002 <lboyd002@ucr.edu>
2015-05-14 12:36:24 -0700

    piping works

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -13,10 +13,13 @@ using namespace std;
 //checks for comments and if stuff is after comments, it deletes it
 void checkcomm(string &str){
 	size_t comment = str.find("#");
+	//cout << "THis is pos of comment: " << comment << endl;
+	str[comment] = '\0';
 	if(comment != string :: npos)
-		str.erase(comment);
+		str.erase(str.begin()+comment, str.end());
+	//cout << "This is str: " << str << endl; 
 }
-
+void tokenize(char *cmnd, char **argv);
 void forkPipes(char **argv, char **afterPipe);
 void simpleFork(char **argv);
 void getPipes(char** argv);
@@ -44,11 +47,10 @@ int main(/*int argc, char *argv[]*/){	//couldn't get argv to work
 	
 	//execute
 	while(1){
-		
 		string input;
-		char cmndline[1024];
-		
-		
+		char buf[1000];
+		//char **argv;
+		//argv = new char*[1024];
 		
 		cout << login << "@" << host << "$ ";
 		
@@ -62,48 +64,46 @@ int main(/*int argc, char *argv[]*/){	//couldn't get argv to work
 		//check for comments
 		checkcomm(input);
 
+		strcpy(buf, input.c_str());
+		
 		//check if user inputs exit	
 		if( input.find("exit") != string::npos )
-			exit(0);	
-				
-
-		//change input from string to cstring
-		strcpy(cmndline, input.c_str());
+			exit(1);	
+		
+		//tokenize(buf, argv);
 		
 		//tokenize the commandline
-		char *tok; //*save;
-		char delim[]= " ;&|";
+		//char *tok;// *save;
+		char delim[]= " \t\n";
 		
 		int size=input.size()+1;
 		//vector<int> order;
 		
-		tok=strtok(cmndline, delim);
-			
 		char **argv=new char*[size];
-		
 		int pos=0;
-		while(tok != NULL)
+		argv[pos]=strtok(buf, delim);
+			
+		while(argv[pos] != NULL)
 		{
-			argv[pos]=tok;
 			pos++;
-			tok = strtok(NULL,delim);
+			argv[pos] = strtok(NULL,delim);
 		}
-		argv[pos]=NULL;
+		//argv[pos]=NULL;
 		
 		
-		//for(unsigned i=0; i < order.size(); i++)
-		//	cout << order[i] << ' ';
+		//for(unsigned i=0; argv[i] != NULL ; i++)
+		//	cout << argv[i] << ' ';
 		//simpleFork(argv);
 		
 		getPipes(argv);
 		
-		delete argv;	
+		delete[] argv;	
 	}
 	
 	return 0;
 }
 
-void simpleFork(char **argv){
+void simpleFork(char **argv, bool BG){
 	int status = 0;
 	int i = fork();
 	if(i == -1){
@@ -119,7 +119,7 @@ void simpleFork(char **argv){
 		}
 	}
 	//in parent
-	else if(i > 0){
+	if(! BG){
 		//error check parent
 		if(wait(&status)==-1){
 			perror("Error in wait");
@@ -130,48 +130,58 @@ void simpleFork(char **argv){
 
 void getPipes(char** argv){
 	bool pipeFound = false;
-	
+	//save spots for the beginning arguments and ending arguments
 	char **args = argv;
 	char **nextA = argv;
 	
+	//go through argv and check if pipes are found
+	//if they are found, it goes into function forkpipes
 	for(int i=0; argv[i] != '\0'; i++){
 		if(strcmp(argv[i], "|") == 0){
 			pipeFound=true;
 			argv[i] = '\0';
-			
 			char** beforeA = args;
 			nextA = args + i + 1;
 			forkPipes(beforeA, nextA);
+			break;
 		}
 	}
-	
+	//if pipe isn't found, it goes into the simple fork
 	if(!pipeFound){
-		//TODO
+		bool BG = false;
+		for(int i=0; nextA[i] != '\0'; i++){
+			if(strcmp(nextA[i], "&") == 0)
+				BG=true;
+		}
+		simpleFork(nextA, BG);
 	}
 }
 
 void forkPipes(char **argv, char **afterPipe){
 	int fd[2];
 	int pid;
-	int status;
+	//pipe new file descriptors
 	if(pipe(fd) == -1){
 		perror("Error with pipe.");
 		exit(EXIT_FAILURE);
 	}
-	
 	pid = fork();
 	if(pid == -1){
 		perror("There was an error with piping fork.");
 		exit(1);
 	}
+	//in child
 	else if(pid ==0){
+		//close stdin
 		if(close(fd[0]) == -1){
 			perror("Error with close in piping.");
 			exit(1);
 		}
+		//save stdout
 		if(dup2(fd[1],1)==-1){
 			perror("Error with dup2 in piping.");
 		}
+		//execute
 		if(execvp(argv[0], argv) == -1){
 			perror("There was an error with execvp piping");
 			exit(1);
@@ -180,24 +190,32 @@ void forkPipes(char **argv, char **afterPipe){
 			exit(0);
 		}	
 	}
-	else{
-		if(close(fd[0]) == -1){
-			perror("Error with close in piping.");
-			exit(1);
-		}
-		
-		int fd0 = dup(0);
-		if(fd0 == -1){
-			perror("There was an error with dup.");
-		}
-		if(dup2(fd[0],0) == -1){
-			perror("Error with dup2 in piping.");
-		}
-		while(waitpid(-1, &status, 0) >= 0);
-		getPipes(afterPipe);
-		if(dup2(fd0,0)==-1){
-			perror("Error with dup2 in piping.");
-		}
-		
-	}	
+	
+	//in parent
+	//save stdin
+	int savestdin=dup(0);
+	
+	if((savestdin == -1))
+		perror("There was an error with dup.");
+	//close std out
+	if(close(fd[1]) == -1){
+		perror("Error with close in piping.");
+		exit(1);
+	}
+	//make stdin with dup2	
+	if(dup2(fd[0],0) == -1){
+		perror("Error with dup2 in piping.");
+	}
+
+	//make sure the parent waits
+	if(wait(0) == -1)
+		perror("There was an error in wait.");
+
+	//while(waitpid(-1, &status, 0) >= 0);
+	//check for pipes after we execute
+	getPipes(afterPipe);
+	
+	if(dup2(savestdin,0)==-1){
+		perror("Error with dup2 in piping.");
+	}
 }
